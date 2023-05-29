39c209f748e0407b5b595d11cd75132732c1b6d7
Tructruc <emilien.fieu@etu.univ-tlse2.fr>
2023-05-29 22:14:43 +0200

    Première version

--- src/main/java/exercice/Exercice.java
+++ src/main/java/exercice/Exercice.java
@@ -1,9 +1,29 @@
 package exercice;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Exercice {
     public static List<String> solution(String str, List<Character> ordre) {
-        throw new UnsupportedOperationException("Unimplemented");
+        String[] splited = str.split("\\s+|'|’");
+
+        List<String> result = new ArrayList<>();
+
+        ArrayList<String>[] tab = new ArrayList[ordre.size()+1];
+        for (int i = 0; i < tab.length; i++){
+            tab[i] = new ArrayList<>();
+        }
+
+        boolean found;
+        List<String> notfound = new ArrayList<>();
+        for (String s : splited){
+            tab[(ordre.contains(s.charAt(0)) ? ordre.indexOf(s.charAt(0)) : tab.length-1 )].add(s);
+        }
+
+        for (ArrayList<String> list : tab){
+            result.addAll(list);
+        }
+        result.addAll(notfound);
+        return result;
     }
 }
