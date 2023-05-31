ae3654e7b0556992fa3a04110f3380dc7772bbc1
Tructruc <emilien.fieu@etu.univ-tlse2.fr>
2023-05-31 14:46:00 +0200

    Ajout d’un random word generator pour tester plus facilement

---
+++ random words.py
@@ -0,0 +1,19 @@
+import requests
+
+def get_random_words(num):
+    url = f"https://random-word-api.herokuapp.com/word?number={num}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        words = response.json()
+        return words
+    else:
+        return None
+
+num_words = 1000
+words = get_random_words(num_words)
+
+if words:
+    words_string = " ".join(words)
+    print(words_string)
+else:
+    print("Failed to fetch random words.")
