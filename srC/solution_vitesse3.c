143650a048a89506ca879a74b40e8a21a1ac8f0d
Tructruc <emilien.fieu@etu.univ-tlse2.fr>
2023-06-01 22:14:05 +0200

    Ajout des chiffres et des tests

--- srC/solution_vitesse3.c
+++ srC/solution_vitesse3.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
+/**
+ * Prétraite l'ordre pour éviter de faire une boucle à chaque fois
+ * @param ordre  ordre à prétraiter
+ * @param tailleOrdre taille de l'ordre
+ * @return tableau de taille 128, avec pour chaque caractère, sa position dans l'ordre (si il n'est pas dans l'ordre, on met a la taille maximale)
+ */
 int* preprocessOrdre(char* ordre, int tailleOrdre){
     int* ordrePreprocess = (int*)malloc(sizeof(int)*128);
+
     for (int i = 0; i < 128; i++) {
         ordrePreprocess[i] = tailleOrdre;
     }
@@ -12,47 +18,60 @@ int* preprocessOrdre(char* ordre, int tailleOrdre){
     }
 
     return ordrePreprocess;
-
 }
 
 void solution(char** output, int tailleOutput, char* input, char* ordre, int tailleOrdre) {
-
-
+    // Initialisation de la liste de listes de mots
     char*** arrayLists = (char***)malloc((tailleOrdre + 1) * sizeof(char*));
-    int* word_count = (int*)calloc((tailleOrdre + 1), sizeof(int));
     for (int i = 0; i < tailleOrdre + 1; i++) {
         arrayLists[i] = (char**)malloc(sizeof(char*)*tailleOutput);
     }
+    int* word_count = (int*)calloc((tailleOrdre + 1), sizeof(int)); // Nombre de mots par liste
 
+    // Prétraitement de l'ordre, pour éviter de faire une boucle à chaque fois
     int* ordrePreprocessed = preprocessOrdre(ordre, tailleOrdre);
 
+    // Initialisation des variables de parcours de la chaine de caractères
     int start = 0;
     int end = 0;
     int length = strlen(input);
 
     while (end < length) {
-         if ((input[end] < 'a' || input[end] > 'z') && (input[end] < 'A' || input[end] > 'Z')) { // si le caractère n'est pas une lettre
+        // si le caractère n'est pas une lettre
+         if ((input[end] < 'a' || input[end] > 'z') && (input[end] < 'A' || input[end] > 'Z') && (input[end] < '0' || input[end] > '9')) {
             if (end - start > 0) { // empèche les mots vides
                 char* word = (char*)malloc((end - start + 1) * sizeof(char));
-                strncpy(word, input + start, end - start); // copie le mot dans word, string + start est l'adresse du premier caractère du mot (on avance dans la chaine de caractère) et end - start est la longueur du mot
-                word[end - start] = '\0'; // ajoute le caractère de fin de chaine
 
-                arrayLists[ordrePreprocessed[word[0]]][word_count[ordrePreprocessed[word[0]]]] = word; // ajoute le mot à la liste
+                // copie le mot dans word, string + start est l'adresse du premier caractère du mot (on avance dans la chaine de caractère) et end - start est la longueur du mot
+                strncpy(word, input + start, end - start);
+
+                // ajoute le caractère de fin de chaine
+                word[end - start] = '\0';
+
+                // ajoute le mot à la liste correspondante
+                arrayLists[ordrePreprocessed[word[0]]][word_count[ordrePreprocessed[word[0]]]] = word;
                 word_count[ordrePreprocessed[word[0]]]++;
             }
             start = end + 1;
         }
         end++;
     }
+    // ajoute le dernier mot (si il n’est pas vide)
     if (end - start > 0) {
         char* word = (char*)malloc((end - start + 1) * sizeof(char));
+
+        // copie le mot dans word, string + start est l'adresse du premier caractère du mot (on avance dans la chaine de caractère) et end - start est la longueur du mot
         strncpy(word, input + start, end - start);
+
+        // ajoute le caractère de fin de chaine
         word[end - start] = '\0';
 
+        // ajoute le mot à la liste correspondante
         arrayLists[ordrePreprocessed[word[0]]][word_count[ordrePreprocessed[word[0]]]] = word; // ajoute le mot à la liste
         word_count[ordrePreprocessed[word[0]]]++;
     }
 
+    // Ajoute les mots dans l'ordre dans le tableau de sortie
     int index = 0;
     for (int i = 0; i < tailleOrdre + 1; i++) {
         for (int j = 0; j < word_count[i]; j++) {
@@ -61,4 +80,13 @@ void solution(char** output, int tailleOutput, char* input, char* ordre, int tai
             index++;
         }
     }
+
+    // Libère la mémoire
+    for (int i = 0; i < tailleOrdre + 1; i++) {
+        free(arrayLists[i]);
+    }
+    free(arrayLists);
+    free(word_count);
+    free(ordrePreprocessed);
+
 }
--- srC/test.c
+++ srC/test.c
@@ -7,12 +7,49 @@
 
 
 int main(int argc, char *argv[]) {
-
-
     int option;
     int nbMots;
-    char *input;
+    int tailleOrdre;
 
+
+    // TEST 1
+
+    char *input1 = "666, the number of the beast";
+    nbMots =  6;
+    char ordre1[] = {'6', 't', 'n', 'o', 'b'};
+    char* result1[nbMots];
+    tailleOrdre =  5;
+
+    solution(result1,nbMots, input1, ordre1, tailleOrdre);
+    char* expected[] = {"666", "the", "the", "number", "of", "beast"};
+    for (int i=0; i<nbMots-1;i++) {
+        if (strcmp(result1[i], expected[i]) != 0) {
+            printf("\033[31mErreur: %s != %s\n\033[0m", result1[i], expected[i]);
+            return 1;
+        }
+    }
+    printf("\033[32mTest 1: OK\n\033[0m");
+
+    // TEST 2
+    char *input2 = "Il fait beau aujourd’hui comme en aout";
+    nbMots =  8;
+    char ordre2[] =  {'f', 'I', 'z', 'u', 'k', 'a', 'b', 'o'};
+    char* result2[nbMots];
+    tailleOrdre =  8;
+
+    solution(result2,nbMots, input2, ordre2, tailleOrdre);
+    char* expected2[] = {"fait", "Il", "aujourd", "aout", "beau", "hui","comme", "en", ""};
+    for (int i=0; i<nbMots-1;i++) {
+        if (strcmp(result2[i], expected2[i]) != 0) {
+            printf("\033[31mErreur: %s != %s\n\033[0m", result2[i], expected2[i]);
+            return 1;
+        }
+    }
+    printf("\033[32mTest 2: OK\n\033[0m");
+
+
+
+    char *input;
     // Process command-line arguments using getopt
     while ((option = getopt(argc, argv, "c:t:")) != -1) {
         switch (option) {
@@ -23,26 +60,32 @@ int main(int argc, char *argv[]) {
                 input = optarg;
                 break;
             default:
-                nbMots =  8;
-                input = "Il fait beau aujourd’hui comme en aout";
+                nbMots =  100;
+                input = "rockets floweret suits reentrance nankins robotizing sulfur wedgies earthen potiches canyon subclaim helotage polliwog knobbed nucleoids postilion flambe sasin lysine touchwood handsets camouflagic valances mutants myologists pans victim pupfishes soberest twined odometer isallobar figurine southeasters ochre misdevelop chopsockies duumvir throatily bricked cinch sinkers hypnogogic musette squirrelly effendi sostenutos dunams dory umbilicals laughable maxims acclaimed saturniid unharnessed acetaldehyde comparableness friskier thornily majuscules paid cowboyed singers nestles aliya fabler scrolling blowpipe pipelining monomaniacal ken deaerated lucidity engorge bethorns refeeding unreflective harpooner oorali rerisen shtickier metaplasm averaged aerosolization algometer narcotically rimmers vitiating dolabrate heliotropism missenses shadowless mesenteron giving urination mendigos heptameters fraises toluol";
         }
     }
 
     if (nbMots == 0 || input == NULL){
-        nbMots =  8;
-        input = "Il fait beau aujourd’hui comme en aout";
+        nbMots =  100;
+        input = "rockets floweret suits reentrance nankins robotizing sulfur wedgies earthen potiches canyon subclaim helotage polliwog knobbed nucleoids postilion flambe sasin lysine touchwood handsets camouflagic valances mutants myologists pans victim pupfishes soberest twined odometer isallobar figurine southeasters ochre misdevelop chopsockies duumvir throatily bricked cinch sinkers hypnogogic musette squirrelly effendi sostenutos dunams dory umbilicals laughable maxims acclaimed saturniid unharnessed acetaldehyde comparableness friskier thornily majuscules paid cowboyed singers nestles aliya fabler scrolling blowpipe pipelining monomaniacal ken deaerated lucidity engorge bethorns refeeding unreflective harpooner oorali rerisen shtickier metaplasm averaged aerosolization algometer narcotically rimmers vitiating dolabrate heliotropism missenses shadowless mesenteron giving urination mendigos heptameters fraises toluol";
     }
 
 
-    int tailleOrdre =  26;
+    tailleOrdre =  26;
     char ordre[] =  {'f', 'I', 'z', 'u', 'k', 'a', 'b', 'o', 'c', 'd', 'e', 'g', 'h', 'i', 'j', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y'};
     char* result[nbMots];
 
+
+
+
+    // Speed test
     struct timespec begin, end;
     clock_gettime(CLOCK_REALTIME, &begin);
 
     solution(result, nbMots, input, ordre, tailleOrdre);
     clock_gettime(CLOCK_REALTIME, &end);
+
+    printf("\nTest de Vitesse\n");
     printf("Word count = %d\n", nbMots);
     printf("Input string lenght = %ld\n", strlen(input));
 
